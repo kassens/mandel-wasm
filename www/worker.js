c4f8fa46ed7db6b3d19352e896acbc6543f60911
Adam Wolff <awolff@gmail.com>
2020-11-01 15:16:41 -0800

    somewhat broken

--- www/worker.js
+++ www/worker.js
@@ -3,19 +3,20 @@ async function init() {
         fetch("../target/wasm32-unknown-unknown/release/mandel_wasm.wasm")
     );
 
-    console.log('fet',"../target/wasm32-unknown-unknown/release/mandel_wasm.wasm");
     onmessage = function (e) {
         let chunk = e.data;
-        let {width, height} = chunk;
-        console.log('run', width, height);
+        console.log('run', chunk);
+        let {step_size, x, y, cols, rows} = chunk;
 
         const buffer_address = instance.exports.IMG_BUFFER.value;
-        instance.exports.render_js();
+        console.log('call js')
+        instance.exports.render_js(step_size, x, y, cols, 140);
+        console.log('did call js')
 
         const arr = new Uint8ClampedArray(
             instance.exports.memory.buffer,
             buffer_address,
-            4 * width * height,
+            4 * cols * rows,
         );
 
         console.log('fe', arr.length, arr)
