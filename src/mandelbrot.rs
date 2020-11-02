3ff97f82880fdfbedeafeea714749a9124a596a9
Adam Wolff <awolff@gmail.com>
2020-11-02 11:24:24 -0800

    Cleanup

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -3,6 +3,7 @@ use std::iter::Enumerate;
 use num_traits;
 use num_rational::Rational64;
 use num_traits::ToPrimitive;
+use num_complex::Complex;
 
 pub fn render<T, F>(iter: Enumerate<IterMut<T>>, store_pixel: F, cols: usize, rows: usize) where
     F: Fn(u8, u8, u8) ->  T {
@@ -10,31 +11,27 @@ pub fn render<T, F>(iter: Enumerate<IterMut<T>>, store_pixel: F, cols: usize, ro
     let x_start = -2 * denom;
     let y_start = denom * -1;
     for (index, pixel) in iter {
-        let cxr = Rational64::new(x_start + (index%cols) as i64, denom);
-        //let cxr = x_start + Rational64::new(3*(index % cols) as i64, 500 as i64);
-        //let cx = -2.0 + step_size * (index % cols) as f64;
-        let cx = cxr.to_f64().unwrap();
+        let cx = Rational64::new(x_start + (index%cols) as i64, denom);
         let y_offset = index / cols;
         if y_offset >= rows {
             break;
         }
-        let cyr = Rational64::new(y_start + y_offset as i64, denom);
-        //let cxr = x_start + Rational64::new(3*(index % cols) as i64, 500 as i64);
-        //let cy = -1.0 + y_offset as f64 * 0.006;
-        let cy = cyr.to_f64().unwrap();
-        let z = calc_z(cx, cy, 3.0);
+        let cy = Rational64::new(y_start + y_offset as i64, denom);
+        let z = calc_z(cx, cy);
         *pixel = store_pixel(z, z, z);
     }
 }
 
 
-fn calc_z<T: Copy + num_traits::Num + std::cmp::PartialOrd>(cx: T, cy: T, clamp: T) -> u8 {
-    let c = num_complex::Complex::new(cx, cy);
-    let mut z = num_complex::Complex::new(cx, cy);
+fn calc_z(cx: Rational64, cy: Rational64) -> u8 {
+    let fx = cx.to_f64().unwrap();
+    let fy = cy.to_f64().unwrap();
+    let c = num_complex::Complex::new(fx, fy);
+    let mut z = c.clone();
 
     let mut i = 0;
-    while i < u8::MAX && z.norm_sqr() < clamp {
-        z = z * z + c;
+    while i < u8::MAX && z.norm_sqr() < 3.0 {
+        z = z*z+c;
         i += 1;
     }
     return i;
