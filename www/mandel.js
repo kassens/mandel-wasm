057fbb1c446c1fa5d367dd6d9f9a44e315129f93
Adam Wolff <awolff@gmail.com>
2020-11-12 12:03:43 -0800

    Initial render works

--- www/mandel.js
+++ www/mandel.js
@@ -1,62 +1,25 @@
 import getRenderer from "./render.js";
 import getTextureUpdater from "./workerpool.js";
-
-window.scaleArr = [1,1]
-
-function fitCanvasSize(gl) {
-  var realToCSSPixels = window.devicePixelRatio;
-
-  // Lookup the size the browser is displaying the canvas in CSS pixels
-  // and compute a size needed to make our drawingbuffer match it in
-  // device pixels.
-  var displayWidth  = Math.floor(gl.canvas.clientWidth  * realToCSSPixels);
-  var displayHeight = Math.floor(gl.canvas.clientHeight * realToCSSPixels);
-
-  // Check if the canvas is not the same size.
-  if (gl.canvas.width  !== displayWidth ||
-      gl.canvas.height !== displayHeight) {
-
-    // Make the canvas the same size
-    gl.canvas.width  = displayWidth;
-    gl.canvas.height = displayHeight;
-  }
-}
-function animate() {
-    return;
-    const segmentDuration = 2000;
-    let start = null;
-    const step = function(ts) {
-        if (start == null) start = ts;
-        let elapsed = ts - start;
-        if (elapsed > segmentDuration) {
-            elapsed = segmentDuration;
-        } else {
-            requestAnimationFrame(step);
-        }
-
-        const sc = 1.0 + 0.25 * elapsed/segmentDuration;
-        render(sc);
-    }
-
-    requestAnimationFrame(step);
-}
 let start = {x:BigInt(-340), y:BigInt(-200)};
 let stepSize = BigInt(160);
 
-// does not queue
-export default async function init(canvas) {
-    const DIMENSIONS = [500, 400];
-    const [width, height] = DIMENSIONS;
-    const [centerX, centerY] = DIMENSIONS.map(x=> BigInt(x/2));
+export default async function init(canvas, width, height) {
+    const [centerX, centerY] = [width, height].map(x=> BigInt(x/2));
 
     const gl = WebGLDebugUtils.makeDebugContext(canvas.getContext("webgl"));
     const {render, copyTexture, resetAnimation} = getRenderer(gl, width, height);
     resetAnimation();
-    const callWorkers = await getTextureUpdater(4, width, height, copyTexture);
     const initialFrame = {x:BigInt(-340), y:BigInt(-200), stepSize : BigInt(160)}
-    let frameInfo = await callWorkers(initialFrame, 0);
+    const initialTasks = getFrameRenderParams(initialFrame, width, height);
+    console.log('initialFrame', initialTasks)
+    const enqueueWork = getTextureUpdater('./wasmwrapper.js', 4);
+    let promises = initialTasks.map(enqueueWork);
+    console.log(promises);
+    promises.map(p => p.then( params => {
+        console.log('in then', params);
+        copyTexture(params.yOffset, params.arr, params.height);
+    }));
 
-    console.log(centerX, centerY)
     const SCALE_FACTOR = BigInt(10);
     const clickHandler = function(e) {
         const x = SCALE_FACTOR * (frameInfo.x + BigInt(e.offsetX)) - centerX;
@@ -76,3 +39,75 @@ export default async function init(canvas) {
     }
 
 }
+
+const NUM_CHUNKS = 16;
+function getFrameRenderParams(frameInfo, width, canvasHeight) {
+    if (canvasHeight % NUM_CHUNKS != 0) {
+        throw new Error("Bad chunk height", canvasHeight/NUM_CHUNKS);
+    }
+    const height = canvasHeight/NUM_CHUNKS;
+    const {x, y, stepSize} = frameInfo;
+    let chunks = Array(NUM_CHUNKS).fill(null).map((_, n) => {
+        let yOffset = n*height;
+        return {x, y: y + BigInt(yOffset), width, height, stepSize, yOffset};
+    });
+
+    let midOut = [];
+    let pa = NUM_CHUNKS/2;
+    let pb = pa+1;
+    var keepGoing;
+    do {
+        keepGoing = false;
+        if (pa >= 0) {
+            midOut.push(chunks[pa--]);
+            keepGoing = true;
+        }
+        if (pb < NUM_CHUNKS) {
+            midOut.push(chunks[pb++]);
+            keepGoing = true;
+        }
+    } while (keepGoing)
+    return midOut;
+}
+
+window.scaleArr = [1,1]
+
+function animate() {
+    return;
+    const segmentDuration = 2000;
+    let start = null;
+    const step = function(ts) {
+        if (start == null) start = ts;
+        let elapsed = ts - start;
+        if (elapsed > segmentDuration) {
+            elapsed = segmentDuration;
+        } else {
+            requestAnimationFrame(step);
+        }
+
+        const sc = 1.0 + 0.25 * elapsed/segmentDuration;
+        render(sc);
+    }
+
+    requestAnimationFrame(step);
+}
+
+function fitCanvasSize(gl) {
+  var realToCSSPixels = window.devicePixelRatio;
+
+  // Lookup the size the browser is displaying the canvas in CSS pixels
+  // and compute a size needed to make our drawing buffer match it in
+  // device pixels.
+  var displayWidth  = Math.floor(gl.canvas.clientWidth  * realToCSSPixels);
+  var displayHeight = Math.floor(gl.canvas.clientHeight * realToCSSPixels);
+
+  // Check if the canvas is not the same size.
+  if (gl.canvas.width  !== displayWidth ||
+      gl.canvas.height !== displayHeight) {
+
+    // Make the canvas the same size
+    gl.canvas.width  = displayWidth;
+    gl.canvas.height = displayHeight;
+  }
+}
+
--- www/workerpool.js
+++ www/workerpool.js
@@ -1,44 +1,55 @@
-export default async function(count, width, height, copyTexture) {
-    let workers = await Promise.all(Array(count).fill(null).map(makeWorker));
-    return async function(frameInfo, textureNum) {
-        const {x, y, stepSize} = frameInfo;
-        const available = workers.length;
-        if (!available) throw new Error("No workers available");
-        const chunkHeight = height/available;
-        const getChunkOffset = n => chunkHeight * n;
-        let buffers = await Promise.all(workers.map(
-                (work, n) => work(x, y + BigInt(getChunkOffset(n)), width, chunkHeight, stepSize)));
-        let offset = textureNum * height;
-        buffers.forEach(
-            (data, n) => copyTexture(offset + getChunkOffset(n), data.arr, chunkHeight));
-        return frameInfo;
+export default function(workerFile, count) {
+    let workQueue = [];
+    let workers = [];
+    let check = () => checkQueue(workQueue, workers);
+    const free = worker => {
+        workers.push(worker);
+        check();
+    }
+    for (var i=0; i< count; i++) {
+        makeWorker(workerFile, free);
+    }
+    return function(params) {
+        const promise = new Promise((resolve, reject) => {
+            workQueue.push({resolve, reject, params});
+        });
+        check();
+        return promise;
     }
 }
 
-function makeWorker() {
-    const worker = new Worker("./worker.js");
-    return new Promise((resolveWorker, rejectWorker) => {
-        let resolver = null;
-        let t = null;
-        worker.onmessage = function(event) {
-            if (event.data == "READY") {
-                resolveWorker(function(x, y, width, height, stepSize) {
-                    t = Date.now();
-                    return new Promise((resolve, reject) => {
-                        // no queuing
-                        if (resolver != null) reject();
-                        resolver = resolve;
-                        worker.postMessage({x, y, width, height, stepSize});
-                    });
-                });
-            } else {
-                console.log('worker took', Date.now() - t);
-                const resolve = resolver;
-                resolver = null;
-                t = null;
-                resolve(event.data);
-            }
+function checkQueue(q, workers) {
+    if (!q.length || !workers.length) return;
+    let {params, resolve, reject} = q.shift(); //order matters for work, use queue behavior
+    let work = workers.pop(); //order doesn't matter for workers
+    work(params, resolve, reject);
+    checkQueue(q, workers);
+}
+
+function makeWorker(workerFile, free) {
+    const worker = new Worker(workerFile);
+    let resolver = null;
+    let t = null;
+    const work = function(params, resolve, reject) {
+        t = Date.now();
+        // no queuing
+        if (resolver != null) reject();
+        resolver = resolve;
+        worker.postMessage(params);
+    }
+    worker.onmessage = function(event) {
+        if (t == null ) {
+            //this must be called by the worker when it initializes
+            if (event.data != "READY") throw new Error("Worker inialization error.");
+        } else {
+            console.log('worker took', Date.now() - t);
+            const resolve = resolver;
+            resolver = null;
+            t = null;
+            resolve(event.data);
         }
-    });
+        free(work);
+    }
+
 }
 
